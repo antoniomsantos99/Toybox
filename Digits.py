4b1d61e61b118e181854103d37bb233578714650
António Santos <antoniomsantos99@gmail.com>
2018-11-22 09:43:42 +0000

    Refactored code
    Looks better and now works for both python2 and python3!

--- Digits.py
+++ Digits.py
@@ -1,18 +1,9 @@
 #Function that counts and prints the number of times a digit occurs in a number.
-def Analyze(number,digit,total):
- count = 0
- for d in number:
-     if d == digit:
-         count +=1
- print str(digit) + " : " + str(count)
+try: input = raw_input
+except NameError: pass
 
+number=input("What's your number? ")
 
-total = 0
-digit = 0
-
-number = raw_input("What number you want? ")
-
-while digit < 10:
-    Analyze(number,str(digit),total)
-    digit += 1
-t = raw_input("Press any key to close")
+for i in range(0,10):
+    print(str(i) + ": " + str(number.count(str(i))))
+t =input("Press any key to close")
